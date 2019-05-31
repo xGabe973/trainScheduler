dd3254e5de21a1ec235a5cc0dc2e6e9630b9db8e
xGabe973 <x973.alvarado@gmail.com>
2019-05-31 13:29:53 -0400

    yay

---
+++ assets/logic.js
@@ -0,0 +1,54 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDI4l5BD9sckfkP59EEsILfAK1f2bM6Ah0",
+    authDomain: "train-hw-f595f.firebaseapp.com",
+    databaseURL: "https://train-hw-f595f.firebaseio.com",
+    projectId: "train-hw-f595f",
+    storageBucket: "train-hw-f595f.appspot.com",
+    messagingSenderId: "43425355554",
+    appId: "1:43425355554:web:bc699981729db423"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+  $("#add-train-btn").on("click", function(event) {
+      event.preventDefault();
+    var name = $("#name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var time = $("#time-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+
+    var newTrain = {
+        name: name,
+        destination: destination,
+        time: time,
+        frequency: frequency,
+    };
+
+    database.ref().push(newTrain);
+    console.log(newTrain);
+
+    alert("Train Successfully Added");
+
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+    var name = childSnapshot.val().name;
+    var destination = childSnapshot.val().destination;
+    var time = childSnapshot.val().time;
+    var frequency = childSnapshot.val().frequency;
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(name),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(),
+        $("<td>").text(),
+    );
+    $("#train-table > tbody").append(newRow);
+});
